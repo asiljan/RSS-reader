b3cac2c58a49854262a452cb2d5ea3a7e17d2bd3
RA\alen.siljan <alen.siljan@greyp.com>
2022-10-18 20:09:01 +0200

    Update feeds screen

--- app/src/main/java/com/alensiljan/rssreader/ui/feed/FeedsScreen.kt
+++ app/src/main/java/com/alensiljan/rssreader/ui/feed/FeedsScreen.kt
@@ -2,12 +2,17 @@ package com.alensiljan.rssreader.ui.feed
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.alensiljan.rssreader.ui.theme.Teal200
 
 @Composable
@@ -20,15 +25,34 @@ fun RssFeeds() {
             )
         },
         content = {
-            Row(verticalAlignment = Alignment.CenterVertically) {
+            //TODO connect with viewModel getFeeds
+            FeedsList(
+                listOf(
+                    Pair("The NY Times", "The New York Times aims to create a space where..."),
+                    Pair("BBC News", "Get latest news"),
+                    Pair("BBC Sport", "Get latest sport news")
+                )
+            )
+        }
+    )
+}
+
+@Composable
+private fun FeedsList(feeds: List<Pair<String, String>>) {
+    LazyColumn {
+        items(feeds) { feed ->
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(12.dp)
+            ) {
 //                Image(painter = painterResource(id = R.drawable.picture_frame), contentDescription = "")
                 Column {
-                    Text("The New York TIme")
-                    Text("The New York Times aims to create a space where...")
+                    Text(feed.first)
+                    Text(feed.second)
                 }
             }
         }
-    )
+    }
 }
 
 @Preview
